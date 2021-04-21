855f208fddb5f353a76fe766a9882ddd3a152e48
John Ketterer <johnmketterer@gmail.com>
2021-04-21 13:52:30 -0500

    added mongodb

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongo = require('mongodb')
+const mongoose = require('mongoose')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +21,8 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+// app.post()
+
+ // monitor server
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
